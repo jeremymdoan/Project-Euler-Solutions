e94559517e0f3342a9bd6f284da8efc59dae2155
Jeremy Doan <jeremy.doan@ihsmarkit.com>
2024-11-20 11:09:37 -0700

    updates problem 54 with correct solution

--- problem54.py
+++ problem54.py
@@ -4,73 +4,97 @@
 from collections import Counter
 
 face_cards = {'A':14, 'K':13, 'Q':12, 'J':11, 'T':10}
+suits = ['C','H','D','S']
+hand_ranks = {
+    "royalFlush": 2000,
+    "straightFlush": 800,
+    "fourKind": 700,
+    "fullHouse": 600,
+    "flush": 500,
+    "straight": 400,
+    "threeKind": 300,
+    "twoPair": 200,
+    "pair": 100,
+    "highCard": 0
+}
 
 def convert_face_cards(a):
     return int(a) if a.isnumeric() else face_cards[a]
 
 def highCards(hand):
     hand_count = Counter(hand[0])
-    values = []
-    for i in hand_count:
-        if hand_count[i]==1:
-            values.append(i)
-    return values
+    value = None
+    if len(hand_count) == 5:
+        value = (max(hand[0]),'highCard')
+    return value
 
 def isPair(hand):
     hand_count = Counter(hand[0])
-    for i in hand_count:
-        if hand_count[i]==2:
-            return [i]
-    return []
+    value = None
+    if len(hand_count) == 4:
+        for i in hand_count:
+            if hand_count[i] == 2:
+                value = (i,'pair')
+    return value
 
 def isTwoPair(hand):
     hand_count = Counter(hand[0])
+    value = None
     pairs = []
     for i in hand_count:
         if hand_count[i]==2:
             pairs.append(i)
-    return pairs
+    if len(pairs) == 2:
+        value = (max(pairs), 'twoPair')
+    return value
 
 def isThreeKind(hand):
     hand_count = Counter(hand[0])
+    value = None
     for i in hand_count:
         if hand_count[i]==3:
-            return [i]
-    return []
+            value = (i,'threeKind')
+    return value 
 
 def isStraight(hand):
+    value = (max(hand[0]),'straight')
     for i in range(4):
         if not hand[0][i+1]-hand[0][i] == 1:
-            return []
-    return hand[0]
+            value = None
+    return value
 
 def isFlush(hand):
+    value = None
     if len(Counter(hand[1]))==1:
-        return hand[0]
-    return []
+        value = (max(hand[0]), 'flush')
+    return value
 
 def isFullHouse(hand):
     hand_count = Counter(hand[0])
+    value = None
     if len(hand_count) == 2:
-        return [isThreeKind(hand_count), isPair(hand_count)]
-    return []
+        value =  (sum(hand[0]),'fullHouse')
+    return value
 
 def isFourKind(hand):
     hand_count = Counter(hand[0])
+    value = None
     for i in hand_count:
         if hand_count[i]==4:
-            return [i]
-    return []
+            value = (i,'fourKind')
+    return value 
 
 def isStraightFlush(hand):
+    value = None
     if isStraight(hand) and isFlush(hand):
-        return hand[0]
-    return []
+        value =  (max(hand[0]),'straightFlush')
+    return value
 
 def isRoyalFlush(hand):
-    if isStraightFlush(hand) and highCards(hand)[0]==14:
-        return hand[0]
-    return []
+    value = None
+    if isStraightFlush(hand) and highCards(hand)==14:
+        value = (2000,'royalFlush')
+    return value
 
 hand_functions = [isRoyalFlush, isStraightFlush, isFourKind, isFullHouse, isFlush, isStraight, isThreeKind, isTwoPair, isPair, highCards]
 
@@ -83,22 +107,34 @@ def handValue(hand):
 
 
 def separateHand(hand):
-    return [[convert_face_cards(a[0]) for a in hand], [a[1] for a in hand]]
+    return [sorted([convert_face_cards(a[0]) for a in hand]), [a[1] for a in hand]]
 
-def compareHands(hand1, hand2):
-    return True
+def main():
+    with open('C:\\Users\\jeremy.doan\\OneDrive - S&P Global\\Code\\Project Euler\\p054_poker.txt', 'r') as rounds:
+        hand1_wins =  0
+        for round in rounds:
+            hands = round.replace('\n', '').split(' ')
+            hand1, hand2 = sorted(hands[:5]), sorted(hands[5:])        
+            hand1_value, hand2_value = handValue(hand1), handValue(hand2)
+            if hand_ranks[hand1_value[1]] > hand_ranks[hand2_value[1]]:
+                hand1_wins += 1
+            elif (hand_ranks[hand1_value[1]] == hand_ranks[hand2_value[1]]):
+                if (hand1_value[0]> hand2_value[0]):
+                    hand1_wins += 1
+                elif hand1_value[0] == hand2_value[0]:
+                    h1, h2 = sorted(separateHand(hand1)[0]), sorted(separateHand(hand2)[0])
+                    while True:
+                        high1, high2 = max(h1), max(h2)            
+                        if high1 > high2:
+                            hand1_wins += 1
+                            break
+                        elif high1 < high2:
+                            break
+                        else:
+                            h1.pop(h1.index(high1))
+                            h2.pop(h2.index(high2))
+        print(hand1_wins)
 
-with open('p054_poker.txt', 'r') as rounds:
-    hand1_wins, hand2_wins = 0, 0
-    for round in rounds:
-        hands = round.replace('\n', '').split(' ')
-        hand1, hand2 = sorted(hands[:5]), sorted(hands[5:])
-        hand1_values, hand2_values = handValue(hand1), handValue(hand2)
-        """
-        if hand1_values[0] > hand2_values[0]:
-            hand1_wins += 1
-        elif hand1_values[0] < hand2_values[0]:
-            hand2_wins += 1
-        elif hand1_values[0] == hand2_values[0]:
-        """
-        print(handValue(hand1), handValue(hand2))
+
+if __name__ == "__main__":
+    main()
