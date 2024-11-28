e9b5f4e104a66f4ef189b88c3230908f1a494133
jeremymdoan <jeremymdoan@gmail.com>
2024-11-28 09:37:24 -0700

    removes test file

--- problem61_tests.py
+++
@@ -1,7 +0,0 @@
-import problem61 as p61
-print(p61.is_cyclic([8128, 2882, 8281]))
-print(p61.is_cyclic([2882, 8228,8281]))
-print(p61.is_cyclic([1221,2123,2334,3443]))
-print(p61.is_cyclic([1221,2123,2334,3412]))
-print(p61.is_cyclic([1221,2123,2334,3443,4345,4512]))
-print(p61.is_cyclic([1221,4345,2123,2334,4512,3443]))
